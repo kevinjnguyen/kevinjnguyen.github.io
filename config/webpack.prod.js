b5a6cdbbd5e18b2c715d11bf984aeba836d971d6
Kevin J Nguyen <kevinjnguyen2@gmail.com>
2020-08-22 17:31:40 -0500

    Copied template.

---
+++ config/webpack.prod.js
@@ -0,0 +1,97 @@
+const paths = require('./paths')
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserJSPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const ImageMinPlugin = require("imagemin-webpack");
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: false,
+  output: {
+    path: paths.build,
+    publicPath: '/',
+    filename: '[name].[contenthash].bundle.js',
+  },
+  plugins: [
+    /**
+     * MiniCssExtractPlugin
+     *
+     * Extracts CSS into separate files.
+     *
+     * Note: style-loader is for development, MiniCssExtractPlugin is for production.
+     * They cannot be used together in the same config.
+     */
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[contenthash].css',
+      chunkFilename: '[id].css',
+    }),
+
+    /**
+     * ImageMinPlugin
+     * 
+     * Optimizes images using compression and different plugins.
+     */
+    new ImageMinPlugin({
+      bail: false, // Ignore errors on corrupted images
+      cache: true,
+      imageminOptions: {
+        plugins: [
+          ["gifsicle", { interlaced: true }],
+          ["jpegtran", { progressive: true }],
+          ["mozjpeg", { quality: 80 }],
+          ["optipng", { optimizationLevel: 5 }],
+          ["svgo", { plugins: [ { removeViewBox: false, },],},],
+        ],
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+
+  /**
+   * Optimization
+   *
+   * Production minimizing of JavaSvript and CSS assets.
+   */
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    // Once your build outputs multiple chunks, this option will ensure they share the webpack runtime
+    // instead of having their own. This also helps with long-term caching, since the chunks will only
+    // change when actual code changes, not the webpack runtime.
+    runtimeChunk: 'single',
+    // This breaks apart commonly shared deps (react, semantic ui, etc) into one shared bundle. React, etc
+    // won't change as often as the app code, so this chunk can be cached separately from app code.
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/](react|react-dom|lodash)[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+})
